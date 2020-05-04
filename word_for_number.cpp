f26f06534eafb41c316e6172eb593fd5c1ae1809
Shahab Hashmi <46817090+snh3003@users.noreply.github.com>
2020-05-04 05:32:48 +0530

    Update word_for_number.cpp

--- word_for_number.cpp
+++ word_for_number.cpp
@@ -1,9 +1,12 @@
+// HackerRank https://www.hackerrank.com/challenges/c-tutorial-conditional-if-else/problem
+
 #include<iostream>
 using namespace std;
 int main(){
     int n;
     string a[]={"Greater than 9","one","two","three","four","five","six","seven","eight","nine"};
     cin>>n;
-    cout<<((n>9) ? a[0]:a[n]);
+    //Ternary operator is used instead of traditional if-else statement as it minizes both our codes nd effort
+    cout<<((n>9) ? a[0]:a[n]); // (statement) ? (Implements this if True) : (Implements this otherwise)
     return 0;
 }
